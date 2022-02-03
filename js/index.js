a6a986f6645d63fee0332d7f85afc4f45afba47d
neel2751 <patelneel1732@gmail.com>
2022-02-03 17:15:26 +0530

    Green

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector(".mobile-menu-button");
+ const menu = document.querySelector(".mobile-menu");
+ const close = document.querySelector(".close");
+ const home = document.querySelector(".topnav");
+ btn.addEventListener("click", () => {
+   menu.classList.toggle("hidden");
+   home.classList.toggle("mt-60");
+ });
+
+ close.addEventListener("click", () => {
+   menu.classList.toggle("hidden");
+   home.classList.toggle("mt-60");
+ });
